82e173aedaa22ae6253f0ce0869d1bd089ba5f1c
Andrew-Anthony <serbanantonio2004@gmail.com>
2026-02-19 16:56:23 +0200

    primele cerinte

---
+++ Seminar01/src/ro/ase/cts/Seminar01.java
@@ -0,0 +1,20 @@
+package ro.ase.cts;
+
+import ro.ase.cts.animale.Lion;
+import ro.ase.cts.animale.Zebra;
+import ro.ase.cts.zoo.Zoo;
+import ro.ase.cts.zoo.ZooKeeper;
+
+public class Seminar01 {
+    public static void main(String[] args) {
+        ZooKeeper keeper = new ZooKeeper("popescu");
+        Zoo zoo = new Zoo("Baneasa",keeper);
+        zoo.addAnimal(new Lion("Leu1",2,"carne",20));
+        zoo.addAnimal(new Lion("Leu2",3,"carne",30));
+        zoo.addAnimal(new Zebra("Zebra1",2,"iarba",200));
+        zoo.addAnimal(new Zebra("Zebra2",3,"iarba",210));
+
+        zoo.feedAllAnimals();
+
+    }
+}
--- Seminar01/src/ro/ase/cts/animale/Animal.java
+++ Seminar01/src/ro/ase/cts/animale/Animal.java
@@ -1,9 +1,14 @@
 package ro.ase.cts.animale;
 
-public class Animal {
-    private String name;
-    private int age;
-    private void eat(){
+public abstract class Animal {
+    protected String name;
+    protected String food;
+    protected int age;
+    public abstract void eat(String nume);
 
+    public Animal(String name, String food, int age) {
+        this.name = name;
+        this.age = age;
+        this.food = food;
     }
 }
