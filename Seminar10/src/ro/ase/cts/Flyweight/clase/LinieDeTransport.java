654b480ef3d9f918c3309c58f84a61503a955292
Andrew-Anthony <serbanantonio2004@gmail.com>
2026-04-30 17:14:50 +0300

    add proxy and flyweight

---
+++ Seminar10/src/ro/ase/cts/Flyweight/clase/LinieDeTransport.java
@@ -0,0 +1,6 @@
+package ro.ase.cts.Flyweight.clase;
+
+public interface LinieDeTransport {
+    void afiseazaAutobuzLinie(Autobuz autobuz);
+    void afiseazaNrMaximDeCalatoriTraseu(Autobuz autobuz);
+}
